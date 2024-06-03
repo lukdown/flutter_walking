bd5defc9c027bee70459db7ffedf0dec26578c26
poula9744 <poula9744@naver.com>
2024-06-03 18:17:23 +0900

    이은빈

--- lib/EunBin/Gps_Map.dart
+++ lib/EunBin/Gps_Map.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+//import 'package:flutter_kakao_map/flutter_kakao_map.dart';
+
 
 class Gps_Map extends StatelessWidget {
   const Gps_Map({super.key});
@@ -9,9 +11,9 @@ class Gps_Map extends StatelessWidget {
       appBar: AppBar(
         centerTitle: true,
         iconTheme: IconThemeData(color: Color(0xff068cd2)),
-        title: Text("결제하기",
+        title: Text("오늘의 산책",
           style: TextStyle(
-              fontWeight: FontWeight.w600, color: Color(0xff068cd2),
+              fontWeight: FontWeight.w600, color: Color(0xff068cd2),fontFamily: "런드리고딕OTF Regular"
           ),
         ),
       ),
@@ -22,3 +24,5 @@ class Gps_Map extends StatelessWidget {
     );
   }
 }
+
+
