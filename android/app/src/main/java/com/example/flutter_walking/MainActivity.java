ca0df88990d1ec85654bd51ba3fcd8e6d35c204a
poula9744 <poula9744@naver.com>
2024-06-04 23:31:31 +0900

    신은 날 버렸다

--- android/app/src/main/java/com/example/flutter_walking/MainActivity.java
+++ android/app/src/main/java/com/example/flutter_walking/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.flutter_walking;
+import io.flutter.embedding.android.FlutterFragmentActivity;
+public class MainActivity extends FlutterFragmentActivity {
 
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
 }
--- lib/EunBin/Gps_Map.dart
+++ lib/EunBin/Gps_Map.dart
@@ -1,28 +1,40 @@
 import 'package:flutter/material.dart';
-//import 'package:flutter_kakao_map/flutter_kakao_map.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+void main() => runApp(const Gps_Map());
 
-class Gps_Map extends StatelessWidget {
-  const Gps_Map({super.key});
+class Gps_Map extends StatefulWidget {
+  const Gps_Map({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<Gps_Map> {
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(37.4979, 127.0276);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        iconTheme: IconThemeData(color: Color(0xff068cd2)),
-        title: Text("오늘의 산책",
-          style: TextStyle(
-              fontWeight: FontWeight.w600, color: Color(0xff068cd2),fontFamily: "런드리고딕OTF Regular"
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('걸음걸음'),
+          backgroundColor: Colors.green[700],
+        ),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
           ),
         ),
       ),
-      body: Container(
-        child: Text("임시"),
-      ),
-
     );
   }
 }
-
-
